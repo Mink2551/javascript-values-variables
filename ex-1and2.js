d4685f7cbf2c63d326c848022c94cb0e547da9df
Mink_2551 <thefirestong@gmail.com>
2024-12-24 14:11:08 +0700

    BP6-Mink

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let name = "Karter"
+let surname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+let resteraunt = "Chef's Table by Chef James"
+let bill = 15000
+let shop = 1500
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+bill = 5000
+shop = 2500
